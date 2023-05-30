53368171a56b4fa62ba53116dd9ba52b5268982c
CN\wb.yangfangting01 <2997978921@qq.com>
2023-05-30 10:04:00 +0800

    selfDialog

--- src/components/Common/SelfDialog.vue
+++ src/components/Common/SelfDialog.vue
@@ -1,29 +1,39 @@
 <template>
     <el-dialog
-        class="public-model" 
+        class="public-model"
+        :width="dialogData.width"
+        :top="dialogData.top||'15vh'" 
         :title="dialogData.title" 
         :visible.sync="dialogFlag" 
-        :close-on-click-modal="false" 
-        :width="dialogData.width"
-        :top="dialogData.top||'15vh'"
+        :close-on-click-modal="false"  
         :modal-append-to-body="dialogData.modalAppendToBody != undefined ? dialogData.modalAppendToBody : true"
         @close="closeDialog"
     >
         <slot v-if="dialogFlag"></slot>
         <span slot="footer" class="dialog-footer">
-            <el-button v-if="dialogData.saveName" size="small" @click="save">{{ dialogData.saveName }}</el-button>
+            <el-button 
+                v-if="dialogData.saveName" 
+                size="small" 
+                @click="save"
+            >
+                {{ dialogData.saveName }}
+            </el-button>
             <el-button 
                 v-if="dialogData.confirmName" 
                 type="primary" 
                 size="small" 
                 @click="confirm"
-            >{{ dialogData.confirmName }}</el-button>
+            >
+                {{ dialogData.confirmName }}
+            </el-button>
             <!--关闭按钮的操作必须写成dialogFlag=false，而不是直接调用closeDialog事件，否则closeDialog之后，会触发@close=closeDialog，导致事件二次触发-->
             <el-button 
                 v-if="dialogData.closeName" 
                 size="small" 
                 @click="closeDialog"
-            >{{dialogData.closeName}}</el-button>
+            >
+                {{dialogData.closeName}}
+            </el-button>
         </span>
     </el-dialog>
 </template>
